0b196a9697665a60c1e882752d84f88c71ac7489
Kenk <kenghin_lim@hotmail.com>
2024-08-08 10:10:52 +0200

    feat: trailblazer banner (#220)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,72 +4,72 @@ import clsx from "clsx";
 import { useTranslation } from "react-i18next";
 import { Footer } from "widgets/footer";
 import { Header } from "widgets/header";
-import * as HomeScreens from 'widgets/01-home-screens';
+import * as HomeScreens from "widgets/01-home-screens";
 import MainLayout from "widgets/layouts/main-layout";
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 import { HomeApiKeys } from "widgets/01-home-screens/lib/types";
 import { blogApi } from "shared/lib/api";
 import { Menu } from "widgets/menu";
-import css from './home.module.scss';
+import css from "./home.module.scss";
 import { useEffect, useState } from "react";
 import { exucuteOnReadyPage } from "shared/lib/utils/browser";
 
 function Home() {
-    const [complete, setComplete] = useState(false);
-    const { t } = useTranslation('home');
+  const [complete, setComplete] = useState(false);
+  const { t } = useTranslation("home");
 
-    useEffect(() => exucuteOnReadyPage(() => setComplete(true)), []);
-    
-    return (    
-        <MainLayout 
-            className={clsx(
-                css.root,
-                complete && css.rootAnimated
-            )}
-            meta={{
-                title: t('Home - Taiko'),   
-                description: t('Taiko is a fully permissionless, Ethereum-equivalent based rollup. Inspired, secured, and sequenced by Ethereum.'),
-                image: "/img/og-image.png",
-                url: "https://taiko.xyz/"
-            }}
-            title="Home – Taiko"
-        >
-            <Menu />
-            <Header fixed />
-            <div className={css.gap}>
-                <HomeScreens.GradientFill>
-                    <HomeScreens.Hero />
-                    <HomeScreens.StickyContent
-                        stickyEl={<HomeScreens.Advantages />}
-                        children={<HomeScreens.About />}
-                    />
-                </HomeScreens.GradientFill>
-                <HomeScreens.Solutions />
-                <HomeScreens.Ecosystem />
-                <HomeScreens.Community />
-                <HomeScreens.Blog />
-                <Footer subscribeBar />
-            </div>
-        </MainLayout>
-    );
-} 
+  useEffect(() => exucuteOnReadyPage(() => setComplete(true)), []);
+
+  return (
+    <MainLayout
+      className={clsx(css.root, complete && css.rootAnimated)}
+      meta={{
+        title: t("Home - Taiko"),
+        description: t(
+          "Taiko is a fully permissionless, Ethereum-equivalent based rollup. Inspired, secured, and sequenced by Ethereum."
+        ),
+        image: "/img/og-image.png",
+        url: "https://taiko.xyz/",
+      }}
+      title="Home – Taiko"
+    >
+      <Menu />
+      <Header fixed />
+      <div className={css.gap}>
+        <HomeScreens.GradientFill>
+          <HomeScreens.Hero />
+          <HomeScreens.StickyContent
+            stickyEl={<HomeScreens.Advantages />}
+            children={<HomeScreens.About />}
+          />
+        </HomeScreens.GradientFill>
+        <HomeScreens.Solutions />
+        <HomeScreens.Ecosystem />
+        <HomeScreens.Community />
+        <HomeScreens.Blog />
+        <HomeScreens.Trailblazer />
+        <Footer subscribeBar />
+      </div>
+    </MainLayout>
+  );
+}
 
 export default Home;
 
 export const getServerSideProps: GetServerSideProps = withTranslation(
-    async (context) => {
-        const queryClient = new QueryClient();
+  async (context) => {
+    const queryClient = new QueryClient();
 
-        await queryClient.prefetchQuery({
-            queryKey: [HomeApiKeys.BLOG],
-            queryFn: () => blogApi.getAll()
-        });
+    await queryClient.prefetchQuery({
+      queryKey: [HomeApiKeys.BLOG],
+      queryFn: () => blogApi.getAll(),
+    });
 
-        return {
-            props: {
-                dehydratedState: dehydrate(queryClient)
-            },
-        }
-    },
-    ["home"] 
-)
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+      },
+    };
+  },
+  ["home"]
+);
--- src/widgets/01-home-screens/ui/04-solutions/index.tsx
+++ src/widgets/01-home-screens/ui/04-solutions/index.tsx
@@ -1,115 +1,112 @@
-import clsx from 'clsx';
-import { useTranslation } from 'next-i18next';
-import React, { useState } from 'react';
-import { Button } from 'shared/components/@buttons/button';
-import { useTranslationObject } from 'shared/lib/hooks/use-translation-object';
-import { MediaQuery } from 'shared/ui/media-query';
-import { HomeScreensEnum, IHomeSolution } from 'widgets/01-home-screens/lib/types';
-import css from './solutions.module.scss';
-import { Player } from '@lottiefiles/react-lottie-player';
+import clsx from "clsx";
+import { useTranslation } from "next-i18next";
+import React, { useState } from "react";
+import { Button } from "shared/components/@buttons/button";
+import { useTranslationObject } from "shared/lib/hooks/use-translation-object";
+import { MediaQuery } from "shared/ui/media-query";
+import {
+  HomeScreensEnum,
+  IHomeSolution,
+} from "widgets/01-home-screens/lib/types";
+import css from "./solutions.module.scss";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 const Solutions: React.FC = () => {
-    const solutions = useTranslationObject<IHomeSolution[]>('solutions.list', 'home');
-    const { t } = useTranslation('home');
-    const [activeSolution, setActiveSolution] = useState(solutions[0]);
-    
-    return (
-        <section
-            className={css.root}
-            id={HomeScreensEnum.SOLUTIONS}
-        >
-            <div className="container">
-                <div className={css.wrapper}>
+  const solutions = useTranslationObject<IHomeSolution[]>(
+    "solutions.list",
+    "home"
+  );
+  const { t } = useTranslation("home");
+  const [activeSolution, setActiveSolution] = useState(solutions[0]);
 
-                    <MediaQuery 
-                        query="(min-width: 769px)"
-                        children={
-                            <div className={css.solution}>
-                                <p className={css.suptitle}>
-                                    {t('solutions.solutions')}
-                                </p>
+  return (
+    <section className={css.root} id={HomeScreensEnum.SOLUTIONS}>
+      <div className="container">
+        <div className={css.wrapper}>
+          <MediaQuery
+            query="(min-width: 769px)"
+            children={
+              <div className={css.solution}>
+                <p className={css.suptitle}>{t("solutions.solutions")}</p>
 
-                                <h2 
-                                    className={css.title}
-                                >
-                                    <span>{activeSolution.title_short}</span> – {activeSolution.extraTitle} 
-                                </h2>
+                <h2 className={css.title}>
+                  <span>{activeSolution.title_short}</span> –{" "}
+                  {activeSolution.extraTitle}
+                </h2>
 
-                                <div className={css.footer}>
-                                    <p className={css.text}>
-                                        {activeSolution.text}
-                                    </p>
-                                </div>
+                <div className={css.footer}>
+                  <p className={css.text}>{activeSolution.text}</p>
+                </div>
 
-                                <div className={css.corner}>
-                                    <Button 
-                                        text={t('solutions.solutions')}
-                                        component="a"
-                                        href="/solutions"
-                                        animated={{
-                                            offset: '20%'
-                                        }}
-                                    />
-                                </div>
-                            </div>
-                        }
+                <div className={css.corner}>
+                  <Button
+                    text={t("solutions.solutions")}
+                    component="a"
+                    href="/solutions"
+                    animated={{
+                      offset: "20%",
+                    }}
+                  />
+                </div>
+              </div>
+            }
+          />
+
+          <ul className={css.text}>
+            {solutions.map((solution, id) => (
+              <li
+                className={clsx(
+                  css.list_item,
+                  solution.title === activeSolution.title && css.list_itemActive
+                )}
+                onMouseEnter={() => setActiveSolution(solution)}
+                key={solution.title}
+              >
+                <div
+                  className={`${css.list_wrapper} aos`}
+                  data-aos-offset="50%"
+                >
+                  <div className={css.list_header}>
+                    <Player
+                      className={css.list_icon}
+                      src={solution.iconLottie}
+                      autoplay
+                      loop
                     />
 
-                    <ul className={css.list}>
-                        {solutions.map((solution, id) => (
-                            <li
-                                className={clsx(
-                                    css.list_item,
-                                    solution.title === activeSolution.title && css.list_itemActive,
-                                )}
-                                onMouseEnter={() => setActiveSolution(solution)}
-                                key={solution.title}
-                            >
-                                <div 
-                                    className={`${css.list_wrapper} aos`}
-                                    data-aos-offset="50%"
-                                >
-                                    <div className={css.list_header}>
-                                        <Player 
-                                            className={css.list_icon}
-                                            src={solution.iconLottie}
-                                            autoplay
-                                            loop
-                                        />
+                    <p className={css.list_index}>
+                      {(id + 1).toString().padStart(2, "0")}
+                    </p>
+                  </div>
 
-                                        <p className={css.list_index}>
-                                            {(id + 1).toString().padStart(2, '0')}
-                                        </p>
-                                    </div>
+                  <h4 className={css.list_title}>
+                    <MediaQuery
+                      query="(min-width: 769px)"
+                      children={<>{solution.title_short}</>}
+                    />
+                    <MediaQuery
+                      query="(max-width: 768px)"
+                      children={
+                        <>
+                          <span>{solution.title_short}</span> –{" "}
+                          {solution.extraTitle}
+                        </>
+                      }
+                    />
+                  </h4>
 
-                                    <h4 className={css.list_title}>
-                                        <MediaQuery 
-                                            query="(min-width: 769px)"
-                                            children={<>{solution.title_short}</>}
-                                        />
-                                        <MediaQuery 
-                                            query="(max-width: 768px)"
-                                            children={<><span>{solution.title_short}</span> – {solution.extraTitle}</>}
-                                        />
-                                    </h4>
-
-                                    
-                                    <MediaQuery 
-                                        query="(max-width: 768px)"
-                                        children={
-                                            <p className={css.list_text}>
-                                                {solution.text}
-                                            </p>
-                                        }
-                                    />
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                  <MediaQuery
+                    query="(max-width: 768px)"
+                    children={<p className={css.list_text}>{solution.text}</p>}
+                  />
                 </div>
-            </div>
-        </section>
-    );
-}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Solutions;
