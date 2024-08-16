3ce3a2aa7fe9eeb8962d5d950642a99906c3affb
Kenk <kenghin_lim@hotmail.com>
2024-08-16 13:19:34 +0700

    Fix layout (#234)

--- src/shared/components/trailblazer/index.tsx
+++ src/shared/components/trailblazer/index.tsx
@@ -4,6 +4,7 @@ import css from "./trailblazer.module.scss";
 interface Props {
   text: string;
   icon: string;
+  logo: string;
   button: React.ReactNode;
   fullwidth?: boolean;
 }
@@ -12,6 +13,7 @@ export const TrailblazerBanner: React.FC<Props> = ({
   button,
   text,
   icon,
+  logo,
   fullwidth,
 }) => {
   return (
@@ -19,9 +21,12 @@ export const TrailblazerBanner: React.FC<Props> = ({
       <div className={fullwidth ? "" : "container"}>
         <div className={css.wrapper} data-class="banner">
           <div className={css.group}>
-            <img className={css.icon} src={icon} alt="Trailblazer" />
-            <p className={css.text}>{text}</p>
-            <div className={css.button}>{button}</div>
+            <img className={css.logo} src={logo} alt="Trailblazer" />
+            <div className={css.group2}>
+              <img className={css.icon} src={icon} alt="Trailblazer" />
+              <p className={css.text}>{text}</p>
+              <div className={css.button}>{button}</div>
+            </div>
           </div>
         </div>
       </div>
